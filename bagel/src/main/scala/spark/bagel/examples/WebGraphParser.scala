42c1049640886af898803a1b697684940250c8d0
Ankur Dave <ankurdave@gmail.com>
2011-08-28 02:09:48 +0000

    Parse and store partitions from URLs

--- bagel/src/main/scala/spark/bagel/examples/WebGraphParser.scala
+++ bagel/src/main/scala/spark/bagel/examples/WebGraphParser.scala
@@ -9,10 +9,13 @@ import spark.bagel.Bagel._
 import scala.collection.mutable.ArrayBuffer
 
 import java.io.{Externalizable,ObjectInput,ObjectOutput,DataOutputStream,DataInputStream}
+import java.net.URL
 
 import com.esotericsoftware.kryo._
 
 import it.unimi.dsi.fastutil.io.BinIO
+import it.unimi.dsi.fastutil.objects.ObjectList
+import it.unimi.dsi.lang.MutableString
 import it.unimi.dsi.util.ImmutableExternalPrefixMap
 import it.unimi.dsi.webgraph.{ImmutableGraph,BVGraph}
 
@@ -20,12 +23,12 @@ object WebGraphParser {
   def main(args: Array[String]) {
     if (args.length < 3) {
       System.err.println(
-        "Usage: WebGraphParser <graphBaseName> <outputFile> <host>")
+        "Usage: WebGraphParser <graphBaseName> <outputDir> <host>")
       System.exit(-1)
     }
 
     val graphBaseName = args(0)
-    val outputFile = args(1)
+    val outputDir = args(1)
     val host = args(2)
     val sc = new SparkContext(host, "WebGraphParser")
 
@@ -41,21 +44,26 @@ object WebGraphParser {
     println("done.")
 
     print("Loading graph...")
-    val graph = BVGraph.load(graphBaseName + ".graph")
+    val graph = BVGraph.load(graphBaseName)
     println("done.")
 
     val numVertices = graph.numNodes()
-    print("Parsing %d nodes into RDD...")
+    print("Parsing %d nodes into RDD...".format(numVertices))
     val vertices: RDD[(Int, PRVertex[Int])] = sc.parallelize(
       for {
         i <- 0 until numVertices
         outEdges = getSuccessors(i, graph).map(
           targetId => new PREdge(targetId))
-      } yield (i, new PRVertex(i, 1.0 / numVertices, outEdges))
+      } yield (i, new PRVertex(i, 1.0 / numVertices, outEdges,
+                               partition = getNodePartition(i, list)))
     )
     println("done.")
 
     println("URL for node with ID 100: %s".format(list.get(100)))
+
+    print("Saving RDD...")
+    vertices.saveAsTextFile(outputDir)
+    println("done.")
   }
 
   def getSuccessors(i: Int, g: BVGraph): ArrayBuffer[Int] = {
@@ -68,5 +76,12 @@ object WebGraphParser {
       d -= 1
     }
     result
+  }
+
+  def getNodePartition(i: Int, list: ObjectList[MutableString]): Int = {
+    val url = list.get(i).toString()
+    val host = new URL(url).getHost()
+    println("Vertex %d has host %s".format(i, host))
+    host.hashCode()
   }
 }
