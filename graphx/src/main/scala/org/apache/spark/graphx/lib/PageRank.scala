7b492d4ceaa0a588c72da6335a13f70db4a2ef79
Ankur Dave <ankurdave@gmail.com>
2014-09-19 11:22:44 -0700

    No unpersist

--- graphx/src/main/scala/org/apache/spark/graphx/lib/PageRank.scala
+++ graphx/src/main/scala/org/apache/spark/graphx/lib/PageRank.scala
@@ -109,8 +109,8 @@ object PageRank extends Logging {
 
       rankGraph.edges.foreachPartition(x => {}) // also materializes rankGraph.vertices
       logInfo(s"PageRank finished iteration $iteration.")
-      prevRankGraph.vertices.unpersist(false)
-      prevRankGraph.edges.unpersist(false)
+      // prevRankGraph.vertices.unpersist(false)
+      // prevRankGraph.edges.unpersist(false)
 
       iteration += 1
     }
